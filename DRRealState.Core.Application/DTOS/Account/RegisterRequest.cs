1e0a28e09a4444f56f443f05eac3a41aec80316f
Jean Carlos Reyes Encarnacion <yacalo45@hotmail.es>
2022-08-26 20:26:41 -0400

    SOme minimum changes

--- DRRealState.Core.Application/DTOS/Account/RegisterRequest.cs
+++ DRRealState.Core.Application/DTOS/Account/RegisterRequest.cs
@@ -13,41 +13,27 @@ namespace DRRealState.Core.Application.DTOS.Account
     /// </summary>
     public class RegisterRequest
     {
-        /// <example>
-        /// Juan
-        /// </example>
+        /// <example> Juan</example>
         [SwaggerParameter(Description = "Name of the User")]
         public string Name { get; set; }
-        /// <example>
-        /// Perez
-        /// </example>
+        /// <example> Perez</example>
         [SwaggerParameter(Description = "LastName of the User")]
         public string LastName { get; set; }
         [JsonIgnore]
         public string PhotoUrl { get; set; }
-        /// <example>
-        /// jperez@example.com
-        /// </example>
+        /// <example> jperez@example.com</example>
         [SwaggerParameter(Description = "Email of the User")]
         public string Email { get; set; }
-        /// <example>
-        /// jperez
-        /// </example>
+        /// <example> jperez</example>
         [SwaggerParameter(Description = "Username of the User")]
         public string Username { get; set; }
-        /// <example>
-        /// 402-8798897-8
-        /// </example>
+        /// <example> 402-8798897-8</example>
         [SwaggerParameter(Description = "Documents of the User")]
         public string Documents { get; set; }
-        /// <example>
-        /// P@ssw0rd
-        /// </example>
+        /// <example> P@ssw0rd</example>
         [SwaggerParameter(Description = "Password of the User")]
         public string Password { get; set; }
-        /// <example>
-        /// +1 809 985 9985
-        /// </example>
+        /// <example> +1 809 985 9985</example>
         [SwaggerParameter(Description = "Phone Number of the User")]
         public string Phone { get; set; }
     }
